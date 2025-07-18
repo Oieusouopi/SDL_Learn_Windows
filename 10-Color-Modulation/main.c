6c6e20b65d69e70b3fbf08dd3fc7031952e80f7c
Oieusouopi <rafasc866@gmail.com>
2025-07-18 12:42:42 -0300

    close e ciclo de evento

--- 10-Color-Modulation/main.c
+++ 10-Color-Modulation/main.c
@@ -3,14 +3,51 @@
 //
 
 
-SDL_Rendere
+#include <SDL.h>
+#include <stdio.h>
+#define HEIGHT 200
+#define WIDTH 200
+
+SDL_Window *window;
 
 bool init();
+void close(int status);
 
 int main() {
-    return 0;
+
+    if (!init()) {
+        printf("Aconteceu algum erro");
+        close(EXIT_FAILURE);
+    }
+
+    while ( true ) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                close(EXIT_SUCCESS);
+            }
+        }
+    }
 }
 
 bool init() {
 
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    window = SDL_CreateWindow("10-Color-Modulation", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Erro janela nao inicializada com sucesso: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+void close(int status) {
+    SDL_DestroyWindow(window);
+    window = NULL;
+
+    SDL_Quit();
+
+    exit(status);
 }
