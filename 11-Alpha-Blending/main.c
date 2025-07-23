109788d379c39a8433716180436278612d23f721
Oieusouopi <rafasc866@gmail.com>
2025-07-23 11:13:57 -0300

    renderizando com textura

--- 11-Alpha-Blending/main.c
+++ 11-Alpha-Blending/main.c
@@ -1,4 +1,5 @@
 #include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 //
 // Created by rafas on 23/07/2025.
@@ -10,14 +11,23 @@ bool init();
 bool loadMedia();
 void close(int status);
 
+SDL_Texture *generateTexture(char path[]);
+
 SDL_Window *window;
 
 SDL_Renderer *renderer;
+
+SDL_Texture *texture;
 
 int main() {
 
     if (!init()) {
         printf("Aconteceu algum erro");
+        close(EXIT_FAILURE);
+    }
+
+    if (!loadMedia()) {
+        printf("Aconteceu algum erro ao carregar as midias");
         close(EXIT_FAILURE);
     }
 
@@ -29,6 +39,11 @@ int main() {
                 close(EXIT_SUCCESS);
             }
         }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture , NULL, NULL);
+
+        SDL_RenderPresent(renderer);
     }
 }
 
@@ -37,13 +52,13 @@ bool init() {
 
     window = SDL_CreateWindow("11-ALPLHA-BLEDING",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
     if (window == NULL) {
-        printf("Aconteceu um erro ao criar a janela: %s", SDL_GetError());
+        printf("Aconteceu um erro ao criar a janela: %s\n", SDL_GetError());
         return false;
     }
 
     renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL) {
-        printf("Aconteceu um erro ao criar o renderizador: %s", SDL_GetError());
+        printf("Aconteceu um erro ao criar o renderizador: %s\n", SDL_GetError());
         return false;
     }
 
@@ -61,4 +76,32 @@ void close(int status) {
     SDL_Quit();
 
     exit(status);
+}
+
+bool loadMedia() {
+
+    texture = generateTexture("../Images/jpg/background_1.jpg");
+    if (texture == NULL) {
+        printf("Erro ao iniciar a textura: %s\n", IMG_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+SDL_Texture *generateTexture(char path[]) {
+    SDL_Texture *newTexture = NULL;
+    SDL_Surface *surface = IMG_Load(path);
+    if (surface == NULL) {
+        printf("Erro ao carregar surface: %s\n", IMG_GetError());
+    }
+
+    newTexture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (newTexture == NULL) {
+        printf("Erro ao criar textura\n");
+    }
+
+    SDL_FreeSurface(surface);
+
+    return newTexture;
 }
