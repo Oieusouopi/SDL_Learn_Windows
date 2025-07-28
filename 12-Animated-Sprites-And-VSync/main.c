03a2aea4b8cd0da5bb6b1c711b78880893508860
Oieusouopi <rafasc866@gmail.com>
2025-07-28 18:49:25 -0300

    sprites

--- 12-Animated-Sprites-And-VSync/main.c
+++ 12-Animated-Sprites-And-VSync/main.c
@@ -14,6 +14,10 @@ SDL_Renderer *renderer;
 
 LTexture *texture;
 
+const int QUADRO_PASSOS = 4;
+
+SDL_Rect gSprites[ 4 ];
+
 bool init();
 void close(int status);
 bool loadMedia();
@@ -30,17 +34,26 @@ int main() {
         close(EXIT_FAILURE);
     }
 
+    int quadros = 0;
     while (true) {
         SDL_Event event;
+
         while (SDL_PollEvent(&event) != 0) {
             if (event.type == SDL_QUIT) {
                 close(EXIT_SUCCESS);
             }
         }
 
-        SDL_RenderClear(renderer);
-        LTexture_Renderer(texture, renderer, NULL, 0,0);
+        SDL_Rect* currentClip = &gSprites[ quadros / 4 ];
+        LTexture_Renderer(texture, renderer, currentClip, (WIDTH - currentClip->w) / 2,(HEIGHT - currentClip->h) / 2);
+
         SDL_RenderPresent(renderer);
+
+        ++quadros;
+
+        if (quadros / 4 >= QUADRO_PASSOS ) {
+            quadros = 0;
+        }
 
     }
 }
@@ -54,7 +67,7 @@ bool init() {
         return false;
     }
 
-    renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (renderer == NULL) {
         printf("Erro ao iniciar renderizador: %s\n", SDL_GetError());
         return false;
@@ -82,9 +95,29 @@ void close(int status) {
 
 bool loadMedia() {
 
-    if (!LTexture_LoadFromFile(texture, renderer, "../Images/alpha100.png")) {
+    if (!LTexture_LoadFromFile(texture, renderer, "../Images/foo.png")) {
         printf("Erro ao carregar o arquivo: %s\n", SDL_GetError());
         return false;
+    } else {
+        gSprites[ 0 ].x = 0;
+        gSprites[ 0 ].y = 0;
+        gSprites[ 0 ].w = 64;
+        gSprites[ 0 ].h = 205;
+
+        gSprites[ 1 ].x = 64;
+        gSprites[ 1 ].y = 0;
+        gSprites[ 1 ].w = 64;
+        gSprites[ 1 ].h = 205;
+
+        gSprites[ 2 ].x = 128;
+        gSprites[ 2 ].y = 0;
+        gSprites[ 2 ].w = 64;
+        gSprites[ 2 ].h = 205;
+
+        gSprites[ 3 ].x = 192;
+        gSprites[ 3 ].y = 0;
+        gSprites[ 3 ].w = 64;
+        gSprites[ 3 ].h = 205;
     }
 
     return true;
