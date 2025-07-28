6df3e551c732fe954ba4c9d8a227bfacebca9c65
Oieusouopi <rafasc866@gmail.com>
2025-07-28 19:42:22 -0300

    metodos no cabecalho LTexture

---
+++ 13-Rotation-And-Flipping/LTexture/LTexture.h
@@ -0,0 +1,26 @@
+//
+// Created by rafas on 28/07/2025.
+//
+#include "SDL.h"
+
+#ifndef LTEXTURE_H
+#define LTEXTURE_H
+
+typedef struct {
+    SDL_Texture *texture;
+    int width;
+    int height;
+} LTexture;
+
+void LTexture_Init(LTexture *lt);
+void LTexture_Free(LTexture *lt);
+
+void LTexture_Renderer(LTexture *lt, SDL_Renderer *renderer, SDL_Rect *clip, int x, int y);
+void LTexture_LoadFromFile(LTexture *lt, const char path[]);
+
+void LTexture_SetColor(LTexture *lt, Uint8 r, Uint8 g, Uint8 b);
+
+void LTexture_SetBlend(LTexture *lt, SDL_BlendMode blending);
+void LTexture_SetAlpha(LTexture *lt, Uint8 alpha);
+
+#endif //LTEXTURE_H
