40c4e48ceb8d5e863a64c2f3b43a10c690029b23
Oieusouopi <rafasc866@gmail.com>
2025-08-05 20:13:07 -0300

    LTextureFromFile

--- 15-mouse-events/LTexture/LTexture.c
+++ 15-mouse-events/LTexture/LTexture.c
@@ -4,13 +4,16 @@
 
 #include "LTexture.h"
 
-void LTexture_Init(LTexture *lt) {
+#include <SDL_image.h>
+#include <stdio.h>
+
+void LTexture_Init(LTexture* lt) {
     lt->texture = NULL;
     lt->width = 0;
     lt->height = 0;
 }
 
-void LTexture_Free(LTexture *lt) {
+void LTexture_Free(LTexture* lt) {
     if (lt->texture != NULL) {
         SDL_DestroyTexture(lt->texture);
         lt->texture = NULL;
@@ -19,3 +22,21 @@ void LTexture_Free(LTexture *lt) {
     }
 }
 
+bool LTexture_LoadFromFile(LTexture* lt, SDL_Renderer* renderer, const char path[]) {
+    SDL_Texture* newTexture;
+
+    SDL_Surface* surface = IMG_Load(path);
+    if (surface == NULL) {
+        printf("Superficie não renderizada %s\n", IMG_GetError());
+        return false;
+    }
+
+    newTexture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (newTexture == NULL) {
+        printf("Não foi possivel criar a textura %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+
+}
